cd1b7ea37eafd98dc8f28eb938233f837b5e33c3
paulyang <xiy076@ucsd.edu>
2017-10-03 22:52:25 -0700

    Rolling Wheel

--- FoodDice/ViewController.swift
+++ FoodDice/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var wheelImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func rotate(_ sender: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 3.0, animations: ({
+            self.rotateOne()
+            self.rotateOne()
+        }))
+    }
+    
+    //MARK: Private functions
+    private func rotateOne() {
+        self.wheelImage.transform = CGAffineTransform.init(rotationAngle: CGFloat(Double.pi))
+        self.wheelImage.transform = CGAffineTransform.init(rotationAngle: CGFloat(0))
+    }
 }
-
