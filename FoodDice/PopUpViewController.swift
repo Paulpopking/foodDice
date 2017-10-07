ee57e8d74070856b6c7102ec66926e7a90e1c5f3
paulyang <xiy076@ucsd.edu>
2017-10-07 11:34:21 -0700

    PopUp Animation Added

--- FoodDice/PopUpViewController.swift
+++ FoodDice/PopUpViewController.swift
@@ -12,6 +12,10 @@ class PopUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue.withAlphaComponent(0.8)
+        
+        self.showAnimation()
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +25,10 @@ class PopUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func closePopUp(_ sender: UIButton) {
+        self.removeAnimation()
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -32,4 +39,25 @@ class PopUpViewController: UIViewController {
     }
     */
 
+    //MARK: Private functions
+    //Functions to show animation
+    private func showAnimation() {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    private func removeAnimation() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0
+        }, completion: {(finished: Bool) in
+            if (finished) {
+                self.view.removeFromSuperview()
+            }
+        })
+    }
 }
